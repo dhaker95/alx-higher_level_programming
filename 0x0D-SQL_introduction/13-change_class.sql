3149243a9aa25436cc63377ebfbd9229cd706236
Dhaker Achour <achour.dhaker@gmail.com>
2023-03-14 02:53:05 -0700

    FIX

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
--- Updates the score of Bob to 10 in the table second_table.
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = "Bob";
+-- Updates bob's score to 10
+
+DELETE FROM second_table
+WHERE score <= 5;
